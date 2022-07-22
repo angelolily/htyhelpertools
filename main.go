9c07f0947ae4b012ed6d7af0f40c582249b16ceb
htyadmin <516209862@qq.com>
2022-07-22 17:07:53 +0800

    feat:<all>:fist commit

--- main.go
+++ main.go
@@ -3,10 +3,7 @@ package main
 import (
 	_ "github.com/angelolily/htyhelpertools/boot"
 	_ "github.com/angelolily/htyhelpertools/router"
-
-	"github.com/gogf/gf/frame/g"
 )
 
 func main() {
-	g.Server().Run()
 }
